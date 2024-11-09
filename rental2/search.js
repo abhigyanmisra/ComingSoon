a9b337de7d7fb13eadbfa22e3d146bd32a59d80d
Sahej Hora <horasahej2203@gmail.com>
2024-11-09 19:14:02 +0530

    ajax and stying on search screen

--- rental2/search.js
+++ rental2/search.js
@@ -9,28 +9,56 @@ function getAllProperties(city) {
             try {
                 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             } catch (e) {
-                alert("BROWSER BROKE");
+                alert("Browser unsupported");
                 return false;
             }
         }
     }
 
-    // Construct the URL with or without the city parameter
     var url = "http://localhost:8083/rentalunsecured/search-properties";
     if (city) {
         url += "?city=" + encodeURIComponent(city);
     }
-    alert("Requesting URL: " + url);
 
     xmlhttp.open("GET", url, true);
     xmlhttp.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
     xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200 && this.responseText != null && this.responseText != "") {
+        if (this.readyState == 4 && this.status == 200 && this.responseText) {
             var res = JSON.parse(this.responseText);
-            var response = JSON.parse(res['response']);
-            console.log(response);
-            // document.getElementById("ipaddress").value = res.response
-        } 
+            var properties = JSON.parse(res['response']);
+            console.log(properties);
+
+            var container = document.getElementById("search_house");
+            container.innerHTML = "";
+
+            for (var i = 0; i < properties.length; i++) {
+                var property = properties[i];
+                var propertyData = JSON.parse(property.data); // Parse the `data` field
+
+                container.innerHTML += `
+                <div class="col-12">
+                    <div class="search_house row">
+                        <div class="col-md-3 house-img">
+                            <img src="${property.image || 'images/house-5.png'}" alt="House Image">
+                        </div>
+                        <div class="col-md-9 house-info">
+                            <p>${propertyData.type || 'Property Type'}</p>
+                            <h3>${propertyData.streetAddress || 'Address'}, ${propertyData.city || ''}</h3>
+                            <p>${propertyData.bedrooms || 'N/A'} bedroom(s) / ${propertyData.bathrooms || 'N/A'} bathroom(s) / ${propertyData.squareFeet || 'N/A'} sq ft</p>
+                            <p>Furnishing: ${propertyData.furnishing || 'Not specified'}</p>
+                            <p>Parking: ${propertyData.carParking || 'N/A'} (${propertyData.carParkingType || 'Not specified'})</p>
+                            <div class="house-price">
+                                <p>Rental Rate: ${propertyData.rentalRate || 'N/A'}</p>
+                                <h4>$${propertyData.rentAmount || '0'} <span>/month</span></h4>
+                            </div>
+                            <p><strong>Terms:</strong> ${propertyData.termsAndConditions || 'Not specified'}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+            
+            }
+        }
     };
 
     xmlhttp.send();
